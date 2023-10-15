b323425037bd6a7152321474ecf93bc48ca64590
Myslitel666 <tretyakov_02@bk.ru>
2023-10-15 19:08:15 +0500

    Problem Page Started

--- TestingLabFront-end/src/Components/Pages/TestingLab3/Problem.tsx
+++ TestingLabFront-end/src/Components/Pages/TestingLab3/Problem.tsx
@@ -1,10 +1,34 @@
+﻿import { Typography } from '@mui/material';
 import React from 'react';
 
+import { useTheme } from '@mui/material/styles';
+
 const Problem: React.FC = () => {
+    const theme = useTheme();
+    const primaryMainColor = theme.palette.primary.main;
+
     return (
-        <div>
-            Problem
-        </div>
+        <>
+            <Typography sx={{
+                'text-align': 'center',
+                color: `${primaryMainColor}`
+            }}>
+                <h2>Основная задача</h2>
+            </Typography>
+            <Typography>
+                <span style={{
+                    color: `${primaryMainColor}`,
+                    marginRight: '10px',
+                    marginLeft: '20px'
+                }}>
+                    Цель:
+                </span>
+                <span>
+                    закрепить теоретические знания и получить практические навыки
+                    в разработке программы тестирования методом базового пути.
+                </span>
+            </Typography>
+        </>
     );
 };
 
